6217c0b840e3ffd2935ea3e96d6af7ca1829e18c
yamineHjr <yasmine.alnajar@gmail.com>
2023-11-15 19:09:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    // origin: "https://schedule-a-meeting-client.vercel.app/",
+    //origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  // console.log(`User Connected: ${socket.id}`);
+
+  // socket.on("join_room", (data) => {
+  //   socket.join(data);
+  // });
+
+  socket.on("send_message", (data) => {
+    //socket.to(data.room).emit("receive_message", data);
+    socket.broadcast.emit("receive_message", data);
+  });
+
+  socket.on("send_final", (data) => {
+    //socket.to(data.room).emit("receive_message", data);
+    socket.broadcast.emit("receive_final", data);
+  });
+
+});
+  
+
+// server.listen(3001, () => {
+server.listen(3000, () => {
+  console.log("SERVER IS RUNNING");
+});
